796ad34eeded63b024047478f708bc7db5c1d36b
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 14:45:32 -0300

    Add project files.

---
+++ ExercicioVogalMaiuscula/Program.cs
@@ -0,0 +1,35 @@
+﻿char[] palavra = new char[10];
+
+for (int i = 0; i < 10; i++)
+{
+    palavra[i] = char.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
+    {
+        switch (palavra[i])
+        {
+            case 'a':
+                palavra[i] = 'A';
+                break;
+            case 'e':
+                palavra[i] = 'E';
+                break;
+            case 'i':
+                palavra[i] = 'I';
+                break;
+            case 'o':
+                palavra[i] = 'O';
+                break;
+            case 'u':
+                palavra[i] = 'U';
+                break;
+            default:
+                Console.WriteLine("Não é vogal");
+                break;
+        }
+    }
+    Console.Write(palavra[i]);
+}
